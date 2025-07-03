8f366f1dd770573ae1ddbe02aa219a41ae8c27ed
ice bear <icebearisreal1@gmail.com>
2025-07-03 17:36:13 +0530

    improvements

--- storage.py
+++ storage.py
@@ -1,6 +1,17 @@
 import random
 
 # This is where all our functions and variables will go.
+#user input for storage: user will enter the activity they want to do.
+userinput = input("What do you want to do? (help for list of activities and commands.)")
+
+def parseinput(userinput):
+    if userinput = 'help':
+        print("------AVAILABLE COMMANDS: ------ \n roll : roll for an item.")
+    elif userinput = "roll":
+        roll()
+    else:
+        print("Unknown command")
+        print("please enter the activity you want to do! (Help for list)")
 
 # Mapping rollable items to numbers
 items= ["diamond", "jam", "ruby"]
